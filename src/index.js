9f7fc1972587119deafe5d1cf4394da83b270f48
Scott Schipke <swschipke@gmail.com>
2019-10-31 09:47:53 -0600

    Add boiler plate for redux and router in main index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import App from './containers/App/App';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools())
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(
+  <Router>
+    <Provider
+      store={store} >
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById('root')
+);
