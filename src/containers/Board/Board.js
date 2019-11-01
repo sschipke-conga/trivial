b29003481cc5ed3c6ed97c9097802327ad6473de
Scott Schipke <swschipke@gmail.com>
2019-11-01 11:17:36 -0600

    Render board

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -12,10 +12,18 @@ export class Board extends Component {
     return (
       <div className="board">
         <div className="board-team-one">
-          <h1>Test</h1>
           <h1 className="team-one-name">{teams[0].name}</h1>
+          <h3 className="score">{teams[0].score}</h3>
         </div>
-        
+        <div className="questions-div">
+          <h3>
+          Current question
+          </h3>
+        </div>
+        <div className="board-team-two">
+          <h1 className="team-one-name">{teams[1].name}</h1>
+          <h3 className="score">{teams[1].score}</h3>
+        </div>
       </div>
     )
   } 
