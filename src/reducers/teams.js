7cd56a3f7d1eb2d2c4e301483603c7e5304d25de
Scott Schipke <swschipke@gmail.com>
2019-10-31 18:35:04 -0600

    Add reducers and default states to store

---
+++ src/reducers/teams.js
@@ -0,0 +1,11 @@
+const teams = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_TEAMS':
+      
+      return action.teams
+    default:
+      return state
+  }
+}
+
+export default teams;
