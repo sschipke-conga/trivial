9f66f7d4c1ce2254fee8cea480d1b89dc91132dd
Scott Schipke <swschipke@gmail.com>
2019-11-01 18:27:53 -0600

    Flex board

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {determineCurrentTeam} from '../../util/helperFuncs'
 import {setCurrentQuestion, setQuestions, increaseTurnCount, setCurrentTeam} from '../../actions/index';
+import './Board.scss'
 export class Board extends Component {
   constructor() {
     super()
@@ -32,10 +33,12 @@ export class Board extends Component {
           <h1 className="team-one-name">{teams[0].name}</h1>
           <h3 className="score">{teams[0].score}</h3>
         </div>
-        <div className="questions-div">
-          <h3>
-          Current question
-          </h3>
+        <div className="question-div">
+          {currentQuestion&&<div className="question-header-div">
+            <h3 className="category">{currentQuestion.category}</h3>
+            <h2 className="question">{currentQuestion.question}</h2>
+            <h3 className="difficulty">{currentQuestion.difficulty}</h3>
+          </div>}
         </div>
         <div className="board-team-two">
           <h1 className="team-one-name">{teams[1].name}</h1>
